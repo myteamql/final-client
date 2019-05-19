14c6482d0336d90e907da2d1d076cb3e7de0c399
Shannon Marquiss <semarqui@calpoly.edu>
2019-05-19 15:15:32 -0700

    persistent drawer add

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import PersistentDrawerLeft from './PersistentDrawerLeft.js';
 
 export default class App extends React.Component {
   constructor(props, context) {
@@ -35,21 +36,8 @@ export default class App extends React.Component {
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <div>{mess}</div>
-        </header>
+          <PersistentDrawerLeft/>
+
       </div>
     );
   }
