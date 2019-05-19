86b2aafead506111475787230cd3c59bd8bd46a2
gfilangeri <gfilange@calpoly.edu>
2019-05-19 13:26:03 -0700

    init client

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div>
+      {{message}}
+    </div>
+  </div>
+</template>
+
+<script>
+  import axios from "axios";
+export default {
+  name: "App",
+  data: () => ({
+    message: ""
+  }),
+  methods: {
+    getMessages() {
+      axios
+              .get("http://localhost:8080/test")
+              .then((response) => {
+                this.message = response.data;
+                this.$emit("success");
+              })
+              .catch(() => {
+              });
+    },
+
+  },
+  created() {
+    this.getMessages();
+  }
+};
+</script>
