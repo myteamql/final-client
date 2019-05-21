a3c11d64688e65a8a0130d700c71fc2da6cd8bc4
Shannon Marquiss <semarqui@calpoly.edu>
2019-05-21 10:58:00 -0700

    navigation

--- src/PersistentDrawerLeft.js
+++ src/PersistentDrawerLeft.js
@@ -134,22 +134,15 @@ class PersistentDrawerLeft extends React.Component {
                     </div>
                     <Divider />
                     <List>
-                        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                        {['Book Room', 'Bookings', 'Owner'].map((text, index) => (
                             <ListItem button key={text}>
                                 <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
                                 <ListItemText primary={text} />
                             </ListItem>
                         ))}
+
                     </List>
-                    <Divider />
-                    <List>
-                        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                            <ListItem button key={text}>
-                                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                                <ListItemText primary={text} />
-                            </ListItem>
-                        ))}
-                    </List>
+                    
                 </Drawer>
                 <main
                     className={classNames(classes.content, {
