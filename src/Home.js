a21af6083e8140582c34f4c652b335fb1df94400
gfilangeri <gfilange@calpoly.edu>
2019-06-09 17:07:49 -0700

    homepage

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import 'rc-slider/assets/index.css';
-import Grid from '@material-ui/core/Grid';
+import { NavLink, Route } from 'react-router-dom';
+import BookRoom from './BookRoom';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-export default class BookRoom extends React.Component {
+export default class Home extends React.Component {
     constructor(props, context) {
         super(props, context);
-
         };
-
-
 
     render ()
     {
         return (
-            <div>
-                <Grid container spacing={8} justify="center" direction={"row"}>
-                    <img src={"MyTeamQL_logo.png"}/>
-                </Grid>
+            <div className="container">
+                    <img src={"sandal.jpg"} width={"75%"}/>
+                    <div className="centered">
+                        <Typography variant="h4" style={{marginBottom: '50px', font: '400 50px/1.3 Oleo Script, Helvetica, sans-serif'}}>
+                        We can't wait to see you.
+                        </Typography>
+                        <NavLink exact to="/bookroom">
+                            <Button variant="outlined">Book Now</Button>
+                        </NavLink>
+                    </div>
+
+                    <div className="content">
+                        <Route exact path="/bookroom" component={BookRoom} />
+                    </div>
             </div>
         );
     }
